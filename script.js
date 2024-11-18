e1a36280c5919c99a038993e625b6e650d125a40
Olanrewaju <olanrewajuoluwasegun51@gmail.com>
2024-11-18 14:54:53 +0100

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstName;
+firstName = "Yinka";
+const lastName = "Hidee";
+console.log(firstName + " " + lastName);
+var firstName = "Silas";
+
+firstName = "Victor";
+console.log(firstName);
+lastName = "Segun";
+console.log(lastName);
